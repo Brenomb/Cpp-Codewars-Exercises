ef9c5ed024bdc2619ac25073d1db92d078ec4685
Breno Maistro <b.maistro@vicsamgroup.it>
2024-12-05 17:37:59 +0100

    Miles per gallon to kilometers per liter

--- mpg to kpl/Converter.cpp
+++ mpg to kpl/Converter.cpp
@@ -4,9 +4,14 @@
 double converter(int mpg)
 {
     return round((mpg * 1.609344) / 4.54609188 * 100.0) / 100.0;
+}
+
+double converterDoneWell(int mpg)
+{
     // mpg into kpl
-    // double imperialGallon = 4.54609188;
-    // double miles = 1.609344;
-    // double result = (mpg * miles) / imperialGallon;
-    // return round(result * 100.0) / 100.0;
+    double imperialGallon = 4.54609188;
+    double miles = 1.609344;
+    double result = (mpg * miles) / imperialGallon;
+    
+    return round(result * 100.0) / 100.0;
 }
