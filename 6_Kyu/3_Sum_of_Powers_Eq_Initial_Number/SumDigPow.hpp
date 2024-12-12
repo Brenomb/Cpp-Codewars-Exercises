2f6ed13c9d60f957e36a56c09d7478e44f9fb076
Breno Maistro <b.maistro@vicsamgroup.it>
2024-12-12 14:21:45 +0100

    6 kyu ex3: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

---
+++ 6_Kyu/3_Sum_of_Powers_Eq_Initial_Number/SumDigPow.hpp
@@ -0,0 +1,8 @@
+#ifndef SUMDIGPOW_HPP
+#define SUMDIGPOW_HPP
+
+#include <vector>
+
+std::vector<unsigned int> sumDigPow(unsigned int a, unsigned int b);
+
+#endif // SUMDIGPOW_HPP
--- main.cpp
+++ main.cpp
@@ -5,19 +5,22 @@
 #include "7_Kyu/3_Breaking_Chocolate_Problem/BreakChocolate.hpp"
 #include "6_Kyu/1_Valid_Braces/ValidBraces.hpp"
 #include "6_Kyu/2_Two_Sum/TwoSum.hpp"
+#include "6_Kyu/3_Sum_of_Powers_Eq_Initial_Number/SumDigPow.hpp"
 
 //Libraries
 #include <iostream>
 #include <stack>
 #include <cmath>
 #include <vector>
+#include <list>
 
 //Namespace
 using namespace std;
 
-int main() {
-    BreakChocolate bc;
-    cout << "hello" << endl;
-    cout << "result: " << bc.break_chocolate(3, 1) << endl;
+int main() {    
+    for (auto num : sumDigPow(1, 100)) {
+        cout << num << " ";
+    }
+    cout << endl;
     return 0;
 }
